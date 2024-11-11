6aacdbffa88351c3777f164cbcdc0e3ef609b23c
Elena Rybakova <lelkya84@gmail.com>
2024-11-11 23:14:30 +0300

    likes done

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,39 @@
 import { Link } from "react-router-dom";
 import { characterType } from "../types/characterType";
+import { useAppDispatch } from "../store/store";
+import { changeLike } from "../store/features/cardsSlice";
 
 type CardProps = {
-    card: characterType;
-}
+  card: characterType;
+};
 
-export function Card ({ card }: CardProps) {
-    return (
-        <Link to={`${card._id}`} className="w-[180px] h-[215px] gap-3 flex flex-col border-[3px] border-solid border-[#CD63FF] p-4 text-ellipsis rounded-2xl">
-            <p className="overflow-hidden whitespace-nowrap text-ellipsis">{card.name}</p>
-            <img src={card.image} alt={card.name} className="w-36 h-36 object-cover rounded-full"/>
-        </Link>
-    )
+export function Card({ card }: CardProps) {
+  const dispatch = useAppDispatch();
+  const handleLikeClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    dispatch(changeLike(card._id));
+  };
+
+  return (
+    <Link
+      to={`${card._id}`}
+      className="w-[180px] h-[230px] gap-2 flex flex-col border-[3px] border-solid border-[#CD63FF] p-4 text-ellipsis rounded-2xl"
+    >
+      <p className="overflow-hidden whitespace-nowrap text-ellipsis">
+        {card.name}
+      </p>
+      <img
+        src={card.image}
+        alt={card.name}
+        className="w-[140px] h-[140px] object-cover rounded-full"
+      />
+      <div onClick={handleLikeClick}>
+        <img
+          src={card.isLiked ? "/like-active.svg" : "/like-not-active.svg"}
+          alt="like"
+          className="w-4 h-4 fill-white scale-image"
+        />
+      </div>
+    </Link>
+  );
 }
