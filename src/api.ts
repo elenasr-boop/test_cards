fe2bcf4fe59718dd6be36d66c6b0020b2c2cfbc6
Elena Rybakova <lelkya84@gmail.com>
2024-11-14 00:01:09 +0300

    S!!!!!!!!!!!!!!!!

--- src/api.ts
+++ src/api.ts
@@ -1,21 +1,9 @@
-const BASE_URL = "http://api.disneyapi.dev/character";
+const BASE_URL = "https://api.disneyapi.dev/character";
 
 export async function getCharacters() {
   const res = await fetch(BASE_URL, {
     method: "GET",
   });
-
-  const data = await res.json();
-  return data;
-}
-
-export async function goToPage(numOfPage: number) {
-  const res = await fetch(
-    `http://api.disneyapi.dev/character?page=${numOfPage}&pageSize=50`,
-    {
-      method: "GET",
-    }
-  );
 
   const data = await res.json();
   return data;
